c0b7fbef6a744bf62970d40cdd113d63f276afd7
Adr150 <adortizgarcia@gmail.com>
2022-11-11 17:52:03 -0600

    :mute: Actualizando gitignore

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Raza)
+class RazaAdmin(admin.ModelAdmin):
+    list_display = ('NombreRaza', 'DescripcionRaza')
+
+@admin.register(Vacuna)
+class VacunaAdmin(admin.ModelAdmin):
+    list_display = ('NombreVacuna', 'DescripcionVacuna')
+    
+@admin.register(Hato)
+class HatoAdmin(admin.ModelAdmin):
+    list_display = ('CodigoHato', 'IngresoHato')
+
+@admin.register(Bovino)
+class BovinoAdmin(admin.ModelAdmin):
+    list_display = ('CodigoBovino', 'RazaBovino', 'HatoBovino', 'display_vacunas', 'Partos', 'UltimoParto', 'FechaCelos')
--- macrofinca/settings.py
+++ macrofinca/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio.apps.InicioConfig',
+    'inventario.apps.InventarioConfig',
+    'registros.apps.RegistrosConfig',
+    'reportes.apps.ReportesConfig'
 ]
 
 MIDDLEWARE = [
